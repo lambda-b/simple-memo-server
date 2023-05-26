f3767276f0c02cf3c87c9ff83edcfeabb4de9201
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-05-26 18:26:57 +0900

    initial commit

---
+++ src/main/java/com/web/memo/SimpleMemoApplication.java
@@ -0,0 +1,13 @@
+package com.web.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/memo/SimpleMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
