f61c64121cea367ccf4f89649648bc0627201473
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-05-27 20:11:23 +0900

    EntityにConstructor,Builderを追加

--- src/main/java/com/web/memo/dao/entity/MemoEntity.java
+++ src/main/java/com/web/memo/dao/entity/MemoEntity.java
@@ -5,29 +5,36 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Table("MEMOS")
 public class MemoEntity {
 
 	/** メモID */
 	@Id
-	private String memoId;
+	private final String memoId;
 
 	/** 順番　*/
-	private long sequence;
+	private final long sequence;
 
 	/** ユーザーID */
-	private String userId;
+	private final String userId;
 
 	/** タイトル */
-	private String title;
+	private final String title;
 
 	/** 内容 */
-	private String content;
+	private final String content;
 
 	/** 更新時刻 */
-	private LocalDateTime updateTimestamp;
+	private final LocalDateTime updateTimestamp;
 
 }
