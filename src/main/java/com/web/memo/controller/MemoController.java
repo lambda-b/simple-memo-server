ab21aa4cf1d28ff45e92a8bb0ab2f3e486e373f3
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-05-28 00:58:39 +0900

    RestControllerに変更

--- src/main/java/com/web/memo/controller/MemoController.java
+++ src/main/java/com/web/memo/controller/MemoController.java
@@ -2,12 +2,11 @@ package com.web.memo.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.web.memo.controller.converter.MemoControllerConverter;
 import com.web.memo.domain.Memo;
@@ -20,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 /**
  * MemoController
  */
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemoController {
 
@@ -29,14 +28,12 @@ public class MemoController {
 
 
 	@GetMapping("/api/memos")
-	@ResponseBody
 	List<MemoApiOut> getAll() {
 		final List<Memo> memos = service.getAll();
 		return MemoControllerConverter.toOut(memos);
 	}
 
 	@PostMapping("/api/save-memos")
-	@ResponseBody
 	void saveMemo(@Validated @RequestBody final List<MemoApiIn> in) {
 		final List<Memo> memos = MemoControllerConverter.toMemo(in);
 		service.saveMemo(memos);
